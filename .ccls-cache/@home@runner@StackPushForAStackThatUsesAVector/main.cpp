fab9bfbdfe28176b97beeb7e684833716a83d9ae
rdavis023 <118138505+rdavis023@users.noreply.github.com>
2024-02-25 23:26:08 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@StackPushForAStackThatUsesAVector/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Stack {
+    private:
+        vector<int> stackVector;
+
+    public:
+        vector<int>& getStackVector() {
+            return stackVector;
+        }
+
+        void printStack() {
+            for (int i = stackVector.size() - 1; i >= 0; i--) {
+                cout << stackVector[i] << endl;
+            }
+        }
+
+        bool isEmpty() {
+            return stackVector.size() == 0;
+        }
+
+        int peek() {
+            if (isEmpty()) {
+                return int();
+            } else {
+                return stackVector[stackVector.size() - 1];
+            }
+        }
+
+        int size() {
+            return stackVector.size();
+        }
+
+    void push(int value) {
+    stackVector.push_back(value);
+}
+   
+};
+
